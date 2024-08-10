a6739504624c3d220014497b6f02c153a230fc2e
Nacho <santoroignacio76@gmail.com>
2024-08-10 04:29:34 -0300

    Modifica archivo server

--- index.js
+++ index.js
@@ -6,12 +6,4 @@ const PORT = 3000
 // Middlewares
 app.use(express.json())
 
-/* app.get('/', (req, res) => {
-    return res.status(200).send({ message: 'Mensaje de respuesta' })
-})
-// Arrancar Servidor
-app.listen(PORT, () => {
-    console.log(`Server is running. http://localhost:${PORT}`)
-})
- */
 module.exports = app;
